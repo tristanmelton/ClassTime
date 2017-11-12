fdbb40ed34cdf341ba93bd957116c67f3e25806a
tristanmelton <30941629+tristanmelton@users.noreply.github.com>
2017-11-12 13:15:05 -0800

    Added Distance function
    Select point
    click flip button
    select second point
    hit calculate distance

--- ClassTime/script/map_init.js
+++ ClassTime/script/map_init.js
@@ -1,6 +1,13 @@
 var map;
 var infowindow;
 
+var markers = [];
+
+var startLoc = null;
+var endLoc = null;
+
+var flip = false;
+
 function initMap(dName) 
 {
 	var ucla = {lat: 34.0689, lng: -118.4452};
@@ -43,13 +50,56 @@ function createMarker(place)
 		map: map,
 		position: place.geometry.location
 	});
-
-	google.maps.event.addListener(marker, 'click', function() {
+	
+	markers.push(marker);
+	
+	google.maps.event.addListener(marker, 'click', function() 
+	{
+		if(!flip)
+		{
+			startLoc = marker;
+		}
+		else
+		{
+			endLoc = marker;
+		}
+		
 		infowindow.setContent(place.name);
 		infowindow.open(map, this);
 	});
 }
-//TODO: FUNCTION CALL DOESN"T DO ANYTHING, NEED TO FIX
+function flipToggler()
+{
+	flip = !flip;
+}
+function calculateDistance()
+{
+	if(startLoc === null || endLoc === null)
+	{
+		alert("Start and end positions cannot be nothing!!!!");
+	}
+	else
+	{
+		var toRad = 3.14159265358979323846264338327950 / 180;
+		var r = 6371e3;
+		var lat1 = startLoc.position.lat() * toRad;
+		var long1 = startLoc.position.lng();
+		var lat2 = endLoc.position.lat() * toRad;
+		var long2 = endLoc.position.lng();
+		
+		var dlat = (lat2 - lat1);
+		var dlong = (long2 - long1) * toRad;
+		
+		var a = Math.sin(dlat/2) * Math.sin(dlat/2) +
+        Math.cos(lat1) * Math.cos(lat2) *
+        Math.sin(dlong/2) * Math.sin(dlong/2);
+		var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+		var d = r * c;
+		alert(d + " meters");
+	}	
+}
+
 function updateMap()
 {
 	var newInfo = document.getElementById('wheretogo').value;
